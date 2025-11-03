61e2b447ea58d2bfaa379e7946295dcbf0000161
Namina <KASIH.ETH@GMAIL.COM>
2025-11-03 20:31:01 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  publicDir: 'public'
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    middlewareMode: false,
+  }
 })
