cde37cecc14e02d4deb06c20245892369c4a0eef
Namina <KASIH.ETH@GMAIL.COM>
2025-11-03 20:57:14 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import NeynarScoreChecker from './NeynarScoreChecker'
 
+// Import Farcaster SDK
+import { sdk } from '@farcaster/miniapp-sdk'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NeynarScoreChecker />
   </React.StrictMode>,
 )
+
+// Call ready() to hide splash screen
+sdk.actions.ready().catch(err => {
+  console.log('Not in Farcaster Mini App context:', err)
+  // App masih bisa jalan normal di browser biasa
+})
